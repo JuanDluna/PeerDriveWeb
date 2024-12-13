2e5ba262af2c995093931f4f9281b92aacdfe834
Diego Zamora Delgadillo <al261855@edu.uaa.mx>
2024-12-13 01:29:18 -0600

    passengerHome

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { PassengerHomeComponent } from './passenger-home/passenger-home.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
+    {path: 'passenger', component: PassengerHomeComponent},
     {path:  '**', pathMatch:"full", redirectTo: 'login'}
 ];
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.ts
+export const environment = {
+    production: false,
+    googleMapsApiKey: 'AIzaSyBmUylyfm0U0a7bFUXw3lDfJhlHl_O9sbA',
+  };
+  
